ace5ea99d40ace84cea8c1706a1f1753cacb0e49
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-12 18:07:32 -0500

    feat: add schema config

--- app.js
+++ app.js
@@ -16,4 +16,19 @@ app.use(session({
 
 app.listen(app.get("port"), () => console.log("Server up"));
 
+const sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/dashboard')
+    } else {
+        next()
+    }
+}
 
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login')
+})
+
+// app.get('/login', )
+app.route('/login').get((req, res) => {
+    res.sendFile(__dirname + '/public/login.html')
+})
