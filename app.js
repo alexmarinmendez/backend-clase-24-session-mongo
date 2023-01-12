9d70a70c8027099b9632cd2a7c44d26518b22fa5
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-12 18:34:12 -0500

    feat: add login and logout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const User = require('./models/User')
 
 const app = express();
 app.set("port", 8080);
@@ -31,4 +32,55 @@ app.get('/', sessionChecker, (req, res) => {
 // app.get('/login', )
 app.route('/login').get((req, res) => {
     res.sendFile(__dirname + '/public/login.html')
+}).post(async (req, res) => {
+    let { username, password } = req.body
+
+    try {
+        let user = await User.findOne({ username }).exec()
+        if (!user) {
+            res.redirect('/login')
+        }
+        if (user.password != password) {
+            res.redirect('/login')
+        }
+        req.session.user = user
+        res.redirect('/dashboard')
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+app.route('/signup').get(sessionChecker, (req, res) => {
+    res.sendFile(__dirname + '/public/signup.html')
+}).post((req, res) => {
+    let user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    user.save((err, docs) => {
+        if (err) {
+            res.redirect('/signup')
+        } else {
+            req.session.user = docs
+            res.redirect('/dashboard')
+        }
+    })
+})
+
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/public/dashboard.html')
+    } else {
+        res.redirect('/login')
+    }
+})
+
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid')
+        res.redirect('/')
+    } else {
+        res.redirect('/login')
+    }
 })
